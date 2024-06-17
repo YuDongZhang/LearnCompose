4a69e01f24d263af385b0f26f10d8453a12e2f4a
zhangyudong <977872239@qq.com>
2024-06-17 17:41:21 +0800

    首次建立最新版本项目

---
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.learncompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.learncompose.ui.theme.LearnComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearnComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                            name = "Android 测试",
+                            modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+//@Composable 如需使函数成为可组合函数，请添加 @Composable 注解。
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+//@Preview  用来预览的
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LearnComposeTheme {
+        Greeting("Android")
+    }
+}
