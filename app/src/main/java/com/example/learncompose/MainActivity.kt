542e5e198f13d4e36b77d64c832bc5868ee26838
zhangyudong <977872239@qq.com>
2024-06-17 17:55:25 +0800

    用法和 flutter 有些类似

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learncompose.ui.theme.LearnComposeTheme
 
@@ -33,10 +37,19 @@ class MainActivity : ComponentActivity() {
 //@Composable 如需使函数成为可组合函数，请添加 @Composable 注解。
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background) ,
+            contentDescription = "你好肾宝")
+        Column {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+            Text(text = "hahha")
+        }
+    }
+
 }
 
 //@Preview  用来预览的
