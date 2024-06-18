a0f71278e95fab7a3d2d6894ff292b1392d9fce5
zhangyudong <977872239@qq.com>
2024-06-18 15:33:58 +0800

    列表没有显示，按照官方文档，目前没有设置数据

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.learncompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -84,8 +87,30 @@ fun MessageCard(msg: Message) {
 
 data class Message(val author: String, val body: String)
 
+@Composable
+fun Conversation(messages:List<Message>){
+    LazyColumn {
+        items(messages){message ->
+            MessageCard(message)
+        }
+    }
+}
 
-@Preview
+//@Preview
+//@Composable
+//fun PreviewConversation() {
+//    ComposeTutorialTheme {
+//        Conversation(SampleData.conversationSample)
+//    }
+//}
+
+//这个没有达到教程的结果，而且发现 @Preview 加一个会复制一份
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     Surface {
