2c4b082da07bb33cfb5f6dd588fc3f2c987aa2e8
zhangyudong <977872239@qq.com>
2024-06-18 11:05:40 +0800

    按照官方文档测试使用了 Material Design

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,7 +15,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -36,12 +39,65 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier.padding(innerPadding)
                     )
                 }
+                //通过实际测试可以看到，下面这个能把上面的覆盖掉。
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
             }
         }
     }
 }
 
-//@Composable 如需使函数成为可组合函数，请添加 @Composable 注解。
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium , shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    Surface {
+        MessageCard(
+            msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+        )
+    }
+
+}
+
+
+    //@Composable 如需使函数成为可组合函数，请添加 @Composable 注解。
 //Modifier 就是一个控制边界的东西
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
